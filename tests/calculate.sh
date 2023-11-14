5deeb4584f1469e12b4e48d62618f9a15ebe6b10
Hannah Driver <Hannah.Driver@oicr.on.ca>
2023-11-14 14:21:31 -0500

    Add regression test files

---
+++ tests/calculate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $1
+
+# - .vcf files have no stochastic content except a header lines with time information
+#   Therefore:
+# - Check md5sums for all types of files, sort
+
+echo ".vcf files:"
+for v in *.vcf; do grep -v Date "$v" | grep -v ^##fileDate | md5sum; done | sort
